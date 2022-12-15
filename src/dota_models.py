58cda440ed3ed32ff7a3dba1b38cfd990f7fe603
Islam Tramov <islam_tramov@mail.ru>
2022-12-15 23:21:20 +0300

    Fixed typings

--- src/dota_models.py
+++ src/dota_models.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List
 
 users_dota_id = {
     'islamtramov': 70040436,
@@ -20,7 +21,7 @@ class player_info:
     assists: int
     last_hits: int
     denies: int
-    items: list[int]
+    items: List[int]
     neutral_item_id: int
     hero_damage: int
     hero_heal: int
@@ -31,14 +32,14 @@ class player_info:
     team: bool
 
 
-def make_player_info(nickname: str, hero_id: str, level: int, kills: int, deaths: int, assists: int, last_hits: int, denies: int, items: list[int], neutral_item_id: int, hero_damage: int, hero_heal: int, tower_damage: int, net_worth: int, gpm: int, xpm: int, team: bool):
+def make_player_info(nickname: str, hero_id: str, level: int, kills: int, deaths: int, assists: int, last_hits: int, denies: int, items: List[int], neutral_item_id: int, hero_damage: int, hero_heal: int, tower_damage: int, net_worth: int, gpm: int, xpm: int, team: bool):
     return player_info(nickname, hero_id, level, kills, deaths, assists, last_hits, denies, items, neutral_item_id, hero_damage, hero_heal, tower_damage, net_worth, gpm, xpm, team)
 
 
 @dataclass
 class match_info:
     id: int
-    players: list[player_info]
+    players: List[player_info]
     start_time: int
     duration: int
     game_mode_id: int
@@ -47,5 +48,5 @@ class match_info:
     radiant_score: int
 
 
-def make_match_info(id: int, players: list[player_info], start_time: int, duration: int, game_mode_id: int, radiant_win: bool, dire_score: int, radiant_score: int):
+def make_match_info(id: int, players: List[player_info], start_time: int, duration: int, game_mode_id: int, radiant_win: bool, dire_score: int, radiant_score: int):
     return match_info(id, players, start_time, duration, game_mode_id, radiant_win, dire_score, radiant_score)
