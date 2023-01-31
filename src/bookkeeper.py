6b87c348ac82a6e9ceab34ae8df0ac0dfebcc084
Islam Tramov <islam_tramov@mail.ru>
2023-01-31 15:41:06 +0300

    Fixed working hours

--- src/bookkeeper.py
+++ src/bookkeeper.py
@@ -15,20 +15,23 @@ def get_user_message(user_id: int):
 
 
 def get_hours_until_end_of_work_message(stop_working_hour: int):
-    hours_until_end_of_work = get_hours_until_end_of_work(stop_working_hour)
-    emoji_hours = get_emoji_number(hours_until_end_of_work)
-    bang_emoji = get_bang_emoji()
+    if is_now_working_time(stop_working_hour):
+        hours_until_end_of_work = get_hours_until_end_of_work(stop_working_hour)
+        emoji_hours = get_emoji_number(hours_until_end_of_work)
+        bang_emoji = get_bang_emoji()
 
-    message = f'{bang_emoji}{emoji_hours}'
-    if (hours_until_end_of_work == 0) or (hours_until_end_of_work > 4):
-        message += ' часов '
-    elif hours_until_end_of_work == 1:
-        message += ' час '
-    elif (hours_until_end_of_work > 1) and (hours_until_end_of_work < 5):
-        message += ' часа '
+        message = f'{bang_emoji}{emoji_hours}'
+        if (hours_until_end_of_work == 0) or (hours_until_end_of_work > 4):
+            message += ' часов '
+        elif hours_until_end_of_work == 1:
+            message += ' час '
+        elif (hours_until_end_of_work > 1) and (hours_until_end_of_work < 5):
+            message += ' часа '
 
-    message += f'и домой{bang_emoji}'
-    return message
+        message += f'и домой{bang_emoji}'
+        return message
+    else:
+        return f'{bang_emoji} время отдыхать {bang_emoji}'
 
 
 def get_days_until_weekend_message():
