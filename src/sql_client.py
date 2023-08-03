74a238fbb1e398da49cf1465529e381b33612192
Islam Tramov <islam_tramov@mail.ru>
2023-08-04 01:33:04 +0300

    Fix db bug

--- src/sql_client.py
+++ src/sql_client.py
@@ -2,29 +2,25 @@ import psycopg2
 from os import getenv
 
 
-# def connect_db():
-#     try:
-#         return psycopg2.connect(
-#             database=getenv("DB_NAME"),
-#             user=getenv("DB_USER"),
-#             password=getenv("DB_PASSWORD"),
-#             host=getenv("DB_HOST")
-#         )
-#     except:
-#         print('Cannot connect to db')
+def connect_db():
+    # try:
+    return psycopg2.connect(
+        database=getenv("DB_NAME"),
+        user=getenv("DB_USER"),
+        password=getenv("DB_PASSWORD"),
+        host=getenv("DB_HOST")
+    )
+    # except:
+        # print('Cannot connect to db')
 
 
 class bald_bookeeper_bot_db_client:
 
     def __init__(self) -> None:
-        self.__connection = psycopg2.connect(
-            database=getenv("DB_NAME"),
-            user=getenv("DB_USER"),
-            password=getenv("DB_PASSWORD")
-        )
+        self.__connection = connect_db()
 
-    # def __del__(self):
-        # self.__connection.close()
+    def __del__(self):
+        self.__connection.close()
 
     def get_username_by_tg_id(self, id: int) -> str:
         sql_query = f'select u.name from users u, telegram_accounts t where t.telegram_id = {id} and t.id = u.id'
